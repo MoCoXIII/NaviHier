712b2051ff658326269e0cbbfdeadbcbebc254cb
Ghxoo <theo.glase@outlook.de>
2026-05-09 12:16:22 +0200

    Raumliste - Hinzufügen einer Raumliste Json Automatisches eintragen in die Listen Json bei Raumerstellung
    TODO:
    - Kommentieren
    - Überarbeiten der UI (Kein Simple Dialog)

--- tools/coordsbestimmung_Funktionen.py
+++ tools/coordsbestimmung_Funktionen.py
@@ -76,6 +76,13 @@ def square (s_x1, s_y1, s_x2, s_y2, plan_path, scale):
                 }
                 add_room(data, plan_path)
 
+                list_data = {
+                        "name": f"{data["names"][0]}",
+                        "path": fr"{str(json_path_floor(plan_path))}",
+                        "rooms_list": f"{rooms_list_pos(plan_path)}"
+                }
+                add_room_list(list_data, plan_path)
+
 # Funktion zum erstellen des Verzeichnisses des circle Raumes // answers: Liste mit den Eingaben; data: speichert das Verzeichnis
 def circle (c_x, c_y, radiuspos_x, radiuspos_y, plan_path, scale):
         answers = dialog(questions_c)
@@ -96,6 +103,13 @@ def circle (c_x, c_y, radiuspos_x, radiuspos_y, plan_path, scale):
                 ]
                 }
                 add_room(data, plan_path)
+
+                list_data = {
+                        "name": f"{data["names"][0]}",
+                        "path": fr"{str(json_path_floor(plan_path))}",
+                        "rooms_list": f"{rooms_list_pos(plan_path)}"
+                }
+                add_room_list(list_data, plan_path)
 
 # Funktion zum erstellen des Verzeichnisses des Wegpunktes // answers: Liste mit den Eingaben; data: speichert das Verzeichnis
 def waypoint (w_x, w_y, plan_path, scale):
@@ -134,29 +148,65 @@ def polygon (p_coords, plan_path):
                 }
                 add_room(data, plan_path)  
 
+                list_data = {
+                        "name": f"{data["names"][0]}",
+                        "path": fr"{str(json_path_floor(plan_path))}",
+                        "rooms_list": fr"{str(rooms_list_pos(plan_path))}"
+                }
+                add_room_list(list_data, plan_path)
+
 # Funktion zum laden, bearbeiten (hinzufügen des neuen Raumes) und speichern der Json Datei // current_data: speichert die aktuelle Json Datei
 def add_room(data, plan_path):
-        with open(fr"{str(json_path(plan_path))}", "r", encoding="utf-8") as file:
+        # Laden in die floor json
+        with open(fr"{str(json_path_floor(plan_path))}", "r", encoding="utf-8") as file:
                 current_data = json.load(file)
 
         current_data["rooms"].append(data)
 
-        with open(fr"{str(json_path(plan_path))}", "w", encoding="utf-8") as file:
+        with open(fr"{str(json_path_floor(plan_path))}", "w", encoding="utf-8") as file:
                 json.dump(current_data, file, indent=4, ensure_ascii=False)
+
+def add_room_list(list_data, plan_path):
+        # laden in die Raumliste json
+        with open(fr"{str(json_path_list(plan_path))}", "r", encoding="utf-8") as file:
+                current_data = json.load(file)
+
+        print(current_data)
+        print(list_data)
+        current_data.append(list_data)
+
+        with open(fr"{str(json_path_list(plan_path))}", "w", encoding="utf-8") as file:
+                json.dump(current_data, file, indent=4, ensure_ascii=False)
+
+def rooms_list_pos(plan_path):
+        with open(fr"{str(json_path_floor(plan_path))}", "r", encoding="utf-8") as file:
+                current_data = json.load(file)
+        
+        return len(current_data["rooms"]) - 1
 
 # Funktion zum laden, bearbeiten (hinzufügen des neuen Wegpunktes) und speichern der Json Datei // current_data: speichert die aktuelle Json Datei
 def add_waypoint(data, plan_path):
-        with open(fr"{str(json_path(plan_path))}", "r", encoding="utf-8") as file:
+        with open(fr"{str(json_path_floor(plan_path))}", "r", encoding="utf-8") as file:
                 current_data = json.load(file)
         
         current_data["waypoints"].append(data)
 
-        with open(fr"{str(json_path(plan_path))}", "w", encoding="utf-8") as file:
+        with open(fr"{str(json_path_floor(plan_path))}", "w", encoding="utf-8") as file:
                 json.dump(current_data, file, indent=4)
 
-def json_path(plan_path):
+def json_path_floor(plan_path):
         path_objekt = Path(plan_path)
         json_folder = path_objekt.parent.parent
-        json_name = f"{json_folder.name}.json"
+        json_folder_name = json_folder.name
+        json_folder = json_folder.as_posix()
+        json_name = f"{json_folder_name}.json"
         json_path = f"{json_folder}/{json_name}"
+        return json_path
+
+def json_path_list(plan_path):
+        path_objekt = Path(plan_path)
+        json_folder = path_objekt.parent.parent.parent.parent
+        json_folder = json_folder.as_posix()
+        json_path = f"{json_folder}/room_list.json"
+        print(json_path)
         return json_path
