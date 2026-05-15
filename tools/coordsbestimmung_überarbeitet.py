d00670438cd932ead0b7bcafe215231e635051fa
Ghxoo <theo.glase@outlook.de>
2026-05-15 16:00:31 +0200

    Erste Eingabe - Anpassen der Titlebar auf Dark, oder Light Mode, je nach Standard auf den PC Systemeinstellungen - Überschrift - Hinzufügen eines Status Label - funktionsfähiges Koordinatenbestimmen vom Square Raum

--- tools/coordsbestimmung_überarbeitet.py
+++ tools/coordsbestimmung_überarbeitet.py
@@ -11,16 +11,8 @@ from coordsbestimmung_Funktionen import square, circle, waypoint, polygon, plan_
 pygame.init()
 
 # erstellen der Positionsvariablen für den square Raum // s_x1, s_y1: erste Koordinate (obere linke Ecke); s_x2, s_y2: zweite Koordinate (untere rechte Ecke)
-s_x1 = 0
-s_y1 = 0
-s_x2 = 0
-s_y2 = 0
-
-# erstellen der Positionsvariablen für den circle Raum // c_x, c_y: Position des Mittelpunktes, radius_pos_x, radius_pos_y: Position des Radiuspunktes
-c_x = 0
-c_y = 0
-radius_pos_x = 0
-radius_pos_y = 0
+s_x = 0
+s_y = 0
 
 # erstellen der Positionsvariablen des Wegpunktes // w_x, w_y: Koordinaten
 w_x = 0
@@ -56,57 +48,96 @@ plan_w, plan_h = plan.get_size()
 screen = pygame.display.set_mode((pygame.display.Info().current_w, pygame.display.Info().current_h), pygame.RESIZABLE)
 pygame.display.set_caption("Raumeditor")
 epw.link_pygame_window(screen)
-hwmd = ctypes.windll.user32.FindWindowW(None, "Raumeditor")             # hwmd (handle window): Window ID      # windll.user32: angeben der Datei: "user32.dll"; FindWindowW: Funktion zum finden eines Fensters
-ctypes.windll.user32.ShowWindow(hwmd, 3)                                # ShowWindow: Funktion zum ändern des Zustandes eines Fensters // 0 = verstecken, 1 = normal anzeigen, 2 = minimieren, 3 = maximieren, 6 = minimieren in die Taskleiste
-pygame.event.pump()                                                     # update von Pygame
+hwnd = pygame.display.get_wm_info()["window"]                                                                           # hwnd (handle window): Window ID      
+ctypes.windll.user32.ShowWindow(hwnd, 3)                                                                                # ShowWindow: Funktion zum ändern des Zustandes eines Fensters // 0 = verstecken, 1 = normal anzeigen, 2 = minimieren, 3 = maximieren, 6 = minimieren in die Taskleiste
+appereance_mode = ctypes.c_int(2)                                                                                       # appereance_mode: Variable zum Speichern des Anzeigemodus der Titelleiste // 0 = Light Mode, 1 = Dark Mode, 2 = Systemstandart
+ctypes.windll.dwmapi.DwmSetWindowAttribute(hwnd, 20, ctypes.byref(appereance_mode), ctypes.sizeof(appereance_mode))     # DwmSetWindowAttribute: Funktion zum Ändern des Anzeigemodus der Titelleiste       # ctypes.byref: Funktion zu Bestimmen des Speicherorts der Variable; ctypes.sizeof: Funktion zum Bestimmen des Speicherplatzes der Variable (int: 4)
+pygame.event.pump()                                                                                                     # update von Pygame
 
-screen_info = pygame.display.Info()
-screen_w, screen_h = screen_info.current_w, screen_info.current_h
-pygame.display.set_mode((screen_w, screen_h))
-faktor = min(screen_w/plan_w, screen_h/plan_h, 1.0)
-new_w, new_h = int(plan_w*faktor), int(plan_h*faktor)
-plan = pygame.transform.scale(plan, (new_w, new_h))
-plan_start_x = (screen_w - new_w) // 2
-plan_start_y = (screen_h - new_h) // 2
-scale = 1000 / int(plan_w*faktor)
+def resize_ui(plan):
+    screen_info = pygame.display.Info()
+    screen_w, screen_h = screen_info.current_w, screen_info.current_h
+    pygame.display.set_mode((screen_w, screen_h), pygame.RESIZABLE)
+    faktor = min(screen_w/plan_w, screen_h/plan_h, 1.0)
+    new_w, new_h = int(plan_w*faktor), int(plan_h*faktor)
+    plan = pygame.transform.scale(plan, (new_w, new_h))
+    plan_start_x = (screen_w - new_w) // 2
+    plan_start_y = (screen_h - new_h) // 2
+    scale = 1000 / int(plan_w*faktor)
+    widget_dic["plan"].config(surface = plan)
+    widget_dic["plan"].place(x = plan_start_x, y = plan_start_y)
+    widget_dic["title"].place(x = plan_start_x + new_w // 2 - widget_dic["title"].width // 2, y = plan_start_y // 2 - widget_dic["title"].height // 2)
+    widget_dic["status_title_label"].place(x = plan_start_x, y = screen_h - plan_start_y // 2 - widget_dic["status_title_label"].height)
+    widget_dic["status_label"].place(x = plan_start_x, y = screen_h - plan_start_y // 2)
+    widget_dic["status_label"].config(min_width = 2 * new_w // 3)
 
-# plazieren des Plans; festlegen der Variable des Plans
-plan_widget = epw.Surface(plan).place(x = plan_start_x, y = plan_start_y)
+    return scale
 
+widget_dic = {
+    "plan": epw.Surface(plan),
+    "title": epw.Label(text="Raumeditor", font=epw.SysFont(font="Calibri", font_size=65)),
+    "status_title_label": epw.Label(text="Status", font=epw.SysFont(font="Calibri", font_size=30), alignment_spacing = 0),
+    "status_label": epw.Label(text="", font=epw.SysFont(font="Calibri", font_size=30),
+                                                        alignment = "left",
+                                                        active_unpressed_background_color = (50, 50, 50), 
+                                                        active_hover_background_color = (50, 50, 50),
+                                                        active_pressed_background_color = (50, 50, 50),
+                                                        top_left_corner_radius = 15, 
+                                                        top_right_corner_radius = 15, 
+                                                        bottom_left_corner_radius = 15, 
+                                                        bottom_right_corner_radius = 15),
+}
+
+resize_ui(plan)
 running = True
 while running:
-
     # färben des Hintergrunds
     screen.fill((47, 91, 235))
 
     for event in pygame.event.get():
-        if event.type == pygame.MOUSEBUTTONDOWN:
+        if event.type == pygame.VIDEORESIZE:
+            scale = resize_ui(plan)
+            plan_start_x = widget_dic["plan"].x
+            plan_start_y = widget_dic["plan"].y
+
+
+
+        elif event.type == pygame.MOUSEBUTTONDOWN:
             if shape == 1:
                 if s_coords_count < 2:
-                    pos == pygame.mouse.get_pos()
-                    s_x1, s_y1 = pos[0] - plan_start_x, pos[1] - plan_start_y
-                    s_coords.append(s_x1 / scale, s_y1 / scale)
-                    s_coords_count += 1
-                    if s_coords_count == 2:
+                    pos = pygame.mouse.get_pos()
+                    s_x, s_y = int((pos[0] - plan_start_x) / scale), int((pos[1] - plan_start_y) / scale)
+                    if s_x >= 0 and s_x <= plan_w and s_y >= 0 and s_y <= plan_h:
+                        s_coords.append(s_x)
+                        s_coords.append(s_y)
+                        s_coords_count += 1
+                        widget_dic["status_label"].config(text = f"Die Koordinate {s_x}, {s_y} wurde hinzugefügt.")
+                    else:
+                        pass
+                    if s_coords_count >= 2:
                         s_submit = True
                 else:
-                    print("Platzhalter")
+                    widget_dic["status_label"].config(text = f"Es wurden bereits zwei Koordinaten hinzugefügt. [{s_coords[0]}, {s_coords[1]}]; [{s_coords[2]}, {s_coords[3]}]")
 
             elif shape == 2:
-                pos == pygame.mouse.get_pos()
+                pos = pygame.mouse.get_pos()
                 p_x, p_y = pos[0] - plan_start_x, pos[1] - plan_start_y
-                p_coords.append(p_x / scale, p_y / scale)
+                p_coords.append(p_x / scale)
+                p_coords.append(p_y / scale)
+                widget_dic["status_label"].config(text = f"Die Koordinate {p_x}, {p_y} wurde hinzugefügt.")
                 p_submit = True
             
             elif shape == 3:
                 if w_coords_count == 0:
-                    pos == pygame.mouse.get_pos()
+                    pos = pygame.mouse.get_pos()
                     w_x, w_y = pos[0] - plan_start_x, pos[1] - plan_start_y
-                    w_coords.append(w_x / scale, w_y / scale)
+                    w_coords.append(w_x / scale)
+                    w_coords.append(w_y / scale)
+                    widget_dic["status_label"].config(text = f"Die Koordinate{s_x1}, {s_y1} wurde hinzugefügt.")
                     if w_coords_count == 1:
                         w_submit = True
                 else:
-                    print("Platzhalter")
+                    widget_dic["status_label"].config(text = f"Es wurde bereits eine Koordinate hinzugefügt. ({w_coords[0]}, {w_coords[1]})")
         
         elif event. type == pygame.KEYDOWN and event.key == pygame.K_BACKSPACE:
             if shape == 1 and s_coords_count > 0:
