71c905785e82d3c8ea9f60be161fb5c05391df4c
Ghxoo <theo.glase@outlook.de>
2026-05-17 14:42:14 +0200

    Raum Auswahl - Hinzufügen von Knöpfen zur Raumauswahl - Funktionsfähigkeit des Polygon Raumes - Outputs bei der Koordinatenbestimmung über das Status Label
    TODO:
    - Möglichkeit zum Abschließen der Koordinatenbestimmung
    - Möglichkeit Koordinaten zu löschen

--- tools/coordsbestimmung_überarbeitet.py
+++ tools/coordsbestimmung_überarbeitet.py
@@ -50,11 +50,11 @@ pygame.display.set_caption("Raumeditor")
 epw.link_pygame_window(screen)
 hwnd = pygame.display.get_wm_info()["window"]                                                                           # hwnd (handle window): Window ID      
 ctypes.windll.user32.ShowWindow(hwnd, 3)                                                                                # ShowWindow: Funktion zum ändern des Zustandes eines Fensters // 0 = verstecken, 1 = normal anzeigen, 2 = minimieren, 3 = maximieren, 6 = minimieren in die Taskleiste
-appereance_mode = ctypes.c_int(2)                                                                                       # appereance_mode: Variable zum Speichern des Anzeigemodus der Titelleiste // 0 = Light Mode, 1 = Dark Mode, 2 = Systemstandart
+appereance_mode = ctypes.c_int(2)                                                                                       # appereance_mode: Variable zum Speichern des Anzeigemodus der Titelleiste // 0 = Light Mode, 1 = Dark Mode, 2 = Systemstandard
 ctypes.windll.dwmapi.DwmSetWindowAttribute(hwnd, 20, ctypes.byref(appereance_mode), ctypes.sizeof(appereance_mode))     # DwmSetWindowAttribute: Funktion zum Ändern des Anzeigemodus der Titelleiste       # ctypes.byref: Funktion zu Bestimmen des Speicherorts der Variable; ctypes.sizeof: Funktion zum Bestimmen des Speicherplatzes der Variable (int: 4)
 pygame.event.pump()                                                                                                     # update von Pygame
 
-def resize_ui(plan):
+def update_ui(plan):
     screen_info = pygame.display.Info()
     screen_w, screen_h = screen_info.current_w, screen_info.current_h
     pygame.display.set_mode((screen_w, screen_h), pygame.RESIZABLE)
@@ -70,13 +70,40 @@ def resize_ui(plan):
     widget_dic["status_title_label"].place(x = plan_start_x, y = screen_h - plan_start_y // 2 - widget_dic["status_title_label"].height)
     widget_dic["status_label"].place(x = plan_start_x, y = screen_h - plan_start_y // 2)
     widget_dic["status_label"].config(min_width = 2 * new_w // 3)
+    widget_dic["room_type_label"].place(x = plan_start_x // 2 - widget_dic["room_type_label"].width // 2, y = plan_start_y)
+    widget_dic["square_button_label"].place(x = plan_start_x // 4 - widget_dic["square_button_label"].width // 2, y = plan_start_y + new_h // 3 - widget_dic["square_button_label"].height)
+    widget_dic["square_button"].place(x = plan_start_x // 4 - widget_dic["square_button_label"].width // 2, y = plan_start_y + new_h // 3)
+    widget_dic["polygon_button_label"].place(x = plan_start_x // 4 - widget_dic["square_button_label"].width // 2, y = plan_start_y + new_h // 2 - widget_dic["polygon_button_label"].height)
+    widget_dic["polygon_button"].place(x = plan_start_x // 4 - widget_dic["square_button_label"].width // 2, y = plan_start_y + new_h // 2)
+    widget_dic["room_type_cancel_button"].place(x = plan_start_x // 4 - widget_dic["square_button_label"].width // 2, y = plan_start_y + 2 * new_h // 3)
 
     return scale
+
+def square_selected():
+    global shape, widget_dic
+    if shape == 0:
+        shape = 1
+        widget_dic["status_label"].config(text = "Quadratischer Raumtyp Ausgewählt")
+
+def polygon_selected():
+    global shape, widget_dic
+    if shape == 0:
+        shape = 2
+        widget_dic["status_label"].config(text = "Polygonaler Raumtyp Ausgewählt")
+
+def room_type_cancel():
+    global shape, widget_dic, s_coords, p_coords, s_coords_count, p_coords_count
+    shape = 0
+    s_coords.clear()
+    p_coords.clear()
+    s_coords_count = 0
+    p_coords_count = 0
+    widget_dic["status_label"].config(text = "Zurücksetzen des Raumtyps")
 
 widget_dic = {
     "plan": epw.Surface(plan),
     "title": epw.Label(text="Raumeditor", font=epw.SysFont(font="Calibri", font_size=65)),
-    "status_title_label": epw.Label(text="Status", font=epw.SysFont(font="Calibri", font_size=30), alignment_spacing = 0),
+    "status_title_label": epw.Label(text="Status", font=epw.SysFont(font="Calibri", font_size=40, bold = True), alignment_spacing = 0, alignment = "left"),
     "status_label": epw.Label(text="", font=epw.SysFont(font="Calibri", font_size=30),
                                                         alignment = "left",
                                                         active_unpressed_background_color = (50, 50, 50), 
@@ -86,9 +113,15 @@ widget_dic = {
                                                         top_right_corner_radius = 15, 
                                                         bottom_left_corner_radius = 15, 
                                                         bottom_right_corner_radius = 15),
+    "room_type_label": epw.Label(text="Raumtyp - / Wegpunktauswahl", font=epw.SysFont(font="Calibri", font_size=40, bold = True)),
+    "square_button_label": epw.Label(text="Quadratischer Raum", font=epw.SysFont(font="Calibri", font_size=30), alignment_spacing = 0, alignment = "left"),
+    "square_button": epw.Button(text="Auswählen", font=epw.SysFont(font="Calibri", font_size=30), command = square_selected),
+    "polygon_button_label": epw.Label(text="Polygon Raum", font=epw.SysFont(font="Calibri", font_size=30), alignment_spacing = 0, alignment = "left"),
+    "polygon_button": epw.Button(text="Auswählen", font=epw.SysFont(font="Calibri", font_size=30), command = polygon_selected),
+    "room_type_cancel_button": epw.Button(text="Abbrechen", font=epw.SysFont(font="Calibri", font_size=30), command = room_type_cancel)
 }
 
-resize_ui(plan)
+update_ui(plan)
 running = True
 while running:
     # färben des Hintergrunds
@@ -96,17 +129,15 @@ while running:
 
     for event in pygame.event.get():
         if event.type == pygame.VIDEORESIZE:
-            scale = resize_ui(plan)
+            scale = update_ui(plan)
             plan_start_x = widget_dic["plan"].x
             plan_start_y = widget_dic["plan"].y
 
-
-
         elif event.type == pygame.MOUSEBUTTONDOWN:
             if shape == 1:
+                pos = pygame.mouse.get_pos()
+                s_x, s_y = int((pos[0] - plan_start_x) / scale), int((pos[1] - plan_start_y) / scale)
                 if s_coords_count < 2:
-                    pos = pygame.mouse.get_pos()
-                    s_x, s_y = int((pos[0] - plan_start_x) / scale), int((pos[1] - plan_start_y) / scale)
                     if s_x >= 0 and s_x <= plan_w and s_y >= 0 and s_y <= plan_h:
                         s_coords.append(s_x)
                         s_coords.append(s_y)
@@ -116,24 +147,29 @@ while running:
                         pass
                     if s_coords_count >= 2:
                         s_submit = True
-                else:
+                elif s_x >= 0 and s_x <= plan_w and s_y >= 0 and s_y <= plan_h:
                     widget_dic["status_label"].config(text = f"Es wurden bereits zwei Koordinaten hinzugefügt. [{s_coords[0]}, {s_coords[1]}]; [{s_coords[2]}, {s_coords[3]}]")
+                else:
+                    pass
 
             elif shape == 2:
                 pos = pygame.mouse.get_pos()
-                p_x, p_y = pos[0] - plan_start_x, pos[1] - plan_start_y
-                p_coords.append(p_x / scale)
-                p_coords.append(p_y / scale)
-                widget_dic["status_label"].config(text = f"Die Koordinate {p_x}, {p_y} wurde hinzugefügt.")
-                p_submit = True
-            
+                p_x, p_y = int((pos[0] - plan_start_x) / scale), int((pos[1] - plan_start_y) / scale)
+                if s_x >= 0 and s_x <= plan_w and s_y >= 0 and s_y <= plan_h:
+                    p_coords.append(p_x / scale)
+                    p_coords.append(p_y / scale)
+                    widget_dic["status_label"].config(text = f"Die Koordinate {p_x}, {p_y} wurde hinzugefügt.")
+                    p_coords_count += 1
+                    if p_coords_count >= 3:
+                        p_submit = True
+
             elif shape == 3:
                 if w_coords_count == 0:
                     pos = pygame.mouse.get_pos()
                     w_x, w_y = pos[0] - plan_start_x, pos[1] - plan_start_y
                     w_coords.append(w_x / scale)
                     w_coords.append(w_y / scale)
-                    widget_dic["status_label"].config(text = f"Die Koordinate{s_x1}, {s_y1} wurde hinzugefügt.")
+                    widget_dic["status_label"].config(text = f"Die Koordinate{s_x}, {s_y} wurde hinzugefügt.")
                     if w_coords_count == 1:
                         w_submit = True
                 else:
